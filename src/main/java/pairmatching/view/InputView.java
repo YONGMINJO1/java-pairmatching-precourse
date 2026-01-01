833e37017b50f104b460e95e2ee37413e5c29065
YONGMINJO1 <iy0594@naver.com>
2026-01-01 23:09:36 +0900

    feat: View 클래스 뼈대 구현
    - InputView 메서드 시그니처 정의
    - OutputView 메서드 시그니처 정의
    - 상세 구현은 추후 진행 예정

---
+++ src/main/java/pairmatching/view/InputView.java
@@ -0,0 +1,21 @@
+package pairmatching.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String readFunction() {
+        //TODO: 구현 예정
+        return null;
+    }
+
+    public static String readCourseAndLevelAndMisstion() {
+        //TODO: 구현 예정
+        return null;
+    }
+
+    public static String readRematchConfirmation() {
+        //TODO: 구현예정
+        return null;
+    }
+}
---
+++ src/main/java/pairmatching/view/OutputView.java
@@ -0,0 +1,30 @@
+package pairmatching.view;
+
+import java.util.List;
+import pairmatching.domain.Pair;
+
+public class OutputView {
+    public static void printMenu() {
+        //TODO: 구현 예정
+    }
+
+    public static void printCoursesAndMissions() {
+        //TODO: 구현 예정
+    }
+
+    public static void printMatchingResult(List<Pair> pairs) {
+        //TODO: 구현 예정
+    }
+
+    public static void printError(String message) {
+        //TODO: 구현 예정
+    }
+
+    public static void printClearComplete() {
+        //TODO: 구현 예정
+    }
+
+    public static void printRematchMessage() {
+        //TODO: 구현 예정
+    }
+}
