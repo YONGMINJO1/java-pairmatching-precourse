01b7a57a50c4c3e213e654b10632a5c0a7647fa2
YONGMINJO1 <iy0594@naver.com>
2026-01-06 22:03:48 +0900

    feat: InputView 구현
    - 기능 선택 입력
    - 과정/레벨/미션 입력
    - 재매칭 확인 입력

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,19 +1,18 @@
 package pairmatching.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
     public static String readFunction() {
-        //TODO: 구현 예정
-        return null;
+        return Console.readLine();
     }
 
     public static String readCourseAndLevelAndMisstion() {
-        //TODO: 구현 예정
-        return null;
+        return Console.readLine();
     }
 
     public static String readRematchConfirmation() {
-        //TODO: 구현예정
-        return null;
+        return Console.readLine();
     }
 }
